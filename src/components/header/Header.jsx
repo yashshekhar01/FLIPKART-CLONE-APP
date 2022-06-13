db41643fc62cb7c23a19838e084978fde567a2ea
yashshekhar01 <yashshekhar2934@gmail.com>
2022-06-13 19:00:15 +0530

    Styling the logo

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,17 +1,44 @@
 
 
-import { AppBar, Toolbar, styled } from '@mui/material';
+import { AppBar, Toolbar, Box, Typography, styled } from '@mui/material';
 
 const StyledHeader = styled(AppBar)`
      background: #2874f0;
      height: 55px;
 `;
 
+const Component = styled(Box)`
+     margin-left: 12%;
+     line-height: 0;
+ `    
+
+const SubHeading = styled(Typography)`
+    font-size: 10px;
+    font-style: italic;
+`    
+const PlusImage = styled('img')({
+    width: 10,
+    height: 10,
+    marginLeft: 4
+})
+
 const Header = () => {
+
+    const logoURL = 'https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/flipkart-plus_8d85f4.png';
+    const subURL = 'https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/plus_aef861.png';
+
     return (
         <StyledHeader>
             <Toolbar>
-
+                <Component>
+                    <img src={logoURL} alt="logo" style={{ width: 75 }} />
+                    <Box style={{ display: 'flex' }}>
+                       <SubHeading>Explore&nbsp;
+                        <Box component="span" style={{ color: '#FFE500'}}>Plus</Box>
+                       </SubHeading>
+                       <PlusImage src={subURL} alt="sub-logo" />
+                    </Box>
+                </Component>
             </Toolbar>
         </StyledHeader>
     )
