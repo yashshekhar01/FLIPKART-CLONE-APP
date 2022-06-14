10056b76b155672bf7cf037dd37c960231ce978a
yashshekhar01 <yashshekhar2934@gmail.com>
2022-06-14 09:37:09 +0530

    Implementing banner

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,11 +1,26 @@
 
+
+
 // components
 import NavBar from './NavBar';
+import Banner from './Banner';
 
+import { Box, styled } from '@mui/material';
+
+const Component = styled(Box)`
+     padding: 10px;
+     background: #F2F2F2;
+`;
 
 const Home = () => {
     return (
-        <NavBar />
+        <>
+            <NavBar />
+            <Component>
+            <Banner />
+            </Component>
+           
+            </>
     )
 }
 
