916078a389418be7f04eb9eff34c33d42af2f4cd
Yash Shekhar <yashshekhar2934@gmail.com>
2022-06-15 18:47:49 +0530

    Implementing login & signup

--- src/components/header/CustomButtons.jsx
+++ src/components/header/CustomButtons.jsx
@@ -1,8 +1,10 @@
-
+import { useState } from "react";
 
 import { Box, Button, Typography, styled } from "@mui/material";
 import { ShoppingCart } from '@mui/icons-material';
 
+// components
+import LoginDialog from "../login/LoginDialog";
 
 const Wrapper = styled(Box)`
     display: flex;
@@ -30,9 +32,16 @@ height: 32px;
 `;
 
 const CustomButtons = () => {
+
+    const [open, setOpen] = useState(false);
+
+    const openDialog = () => {
+        setOpen(true);
+    }
+
     return (
         <Wrapper>
-            <LoginButton variant="contained">Login</LoginButton>
+            <LoginButton variant="contained" onClick={() => openDialog()}>Login</LoginButton>
 
             <Typography style={{ marginTop: 3, width: 135 }}>Become a Seller</Typography>
             <Typography style={{ marginTop: 3 }}>More</Typography>
@@ -41,6 +50,7 @@ const CustomButtons = () => {
                 <ShoppingCart />
                 <Typography>Cart</Typography>
             </Container>
+            <LoginDialog open={open} setOpen={setOpen}/>
         </Wrapper> 
     )
 }
