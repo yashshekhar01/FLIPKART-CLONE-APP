91823833879c38f6eef3c3f54603170104e631a7
Yash Shekhar <yashshekhar2934@gmail.com>
2022-06-18 17:22:30 +0530

    Add & Styles slides of homepage

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -4,6 +4,9 @@ import { useEffect } from 'react';
 // components
 import NavBar from './NavBar';
 import Banner from './Banner';
+import Slide from './Slide';
+import MidSlide from './MidSlide';
+import MidSection from './MidSection';
 
 import { Box, styled } from '@mui/material';
 
@@ -19,8 +22,6 @@ const Home = () => {
     
     const { products } = useSelector(state => state.getProducts)
     
-    console.log(products);
-
     const dispatch = useDispatch();
 
     useEffect(() => {
@@ -32,6 +33,15 @@ const Home = () => {
             <NavBar />
             <Component>
             <Banner />
+            <MidSlide products={products} title="Deal of the Day" timer={true} />
+            <MidSection />
+            <Slide products={products} title="Discounts for You" timer={false} />
+            <Slide products={products} title="Suggested Items" timer={false} />
+            <Slide products={products} title="Top Selection" timer={false} />
+            <Slide products={products} title="Recommended Items" timer={false} />
+            <Slide products={products} title="Trending Offers" timer={false} />
+            <Slide products={products} title="Season's top picks" timer={false} />
+            <Slide products={products} title="Top Deals on Accessories" timer={false} />
             </Component>
            
             </>
